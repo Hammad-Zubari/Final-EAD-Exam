0911c50819b8b09c49e97f938311049060625574
Hammad-Zubari <muhammadhammad.bscsf22@iba-suk.edu.pk>
2025-06-02 23:33:38 -0700

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ const app = express();
 
 
 const db = "mongodb+srv://Hammad:Hammad123.@node.9jjukmv.mongodb.net/?retryWrites=true&w=majority&appName=Node";
+
+// Connect to MongoDB 
+mongoose.connect(db)
+    .then(() => {
+        app.listen(3000);
+        console.log('connect DB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
